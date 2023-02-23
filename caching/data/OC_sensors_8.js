3b31e5f136e4a65fd63d88c1f4adeffc4cf89524
Elinor <elinorco24@gmail.com>
2023-02-23 15:02:51 +0200

    add caching files and data

---
+++ caching/data/OC_sensors_8.js
@@ -0,0 +1,1 @@
+var json_OC_sensors_8 = {"type":"FeatureCollection","name":"OC_sensors_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ caching/data/Soil_Sensors_2.js
@@ -0,0 +1,1 @@
+var json_Soil_Sensors_2 = {"type":"FeatureCollection","name":"Soil_Sensors_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
